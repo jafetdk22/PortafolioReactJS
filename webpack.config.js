50594f61cad5c1f591addaaf3187d8c8a4fe201a
Jafet Solano <65096975+jafetdk22@users.noreply.github.com>
2025-02-09 13:53:26 -0600

    se optimiza
    BundleAnalyzerPlugin se agrega para optimizar

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 module.exports = {
     // ...
     optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
         minimize: true,
     },
+    plugins: [
+        new BundleAnalyzerPlugin(),
+    ],
     // ...
 }; 
